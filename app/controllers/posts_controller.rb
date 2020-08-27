d02b72102c0eda7135b4e8621fb40619c2f33b6d
GabrielaJustine16 <johnsongabriela12@gmail.com>
2020-08-26 22:48:58 -0700

    get posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,19 @@ class PostsController < ApplicationController
 
     #INDEX route for all posts
     get '/posts' do 
+        #leverage the model to get all posts from database
+        @posts = Post.all 
+        #store an array as an instance variable called posts . all then render in erb so you can access
+        #render all posts
+        erb :'posts/index'
 
     end 
 
     #show route for a single post
+
+    #CREATE
+
+    #UPDATE
+
+    #DELETE
 end 
