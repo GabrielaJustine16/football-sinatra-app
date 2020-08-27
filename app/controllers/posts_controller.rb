f38cc20767190f8f8e94986a65d0476369493ff5
GabrielaJustine16 <johnsongabriela12@gmail.com>
2020-08-26 22:38:53 -0700

    adds comments and get posts to the post controller. Add CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+
+    #CRUD
+
+    #READ
+
+    #INDEX route for all posts
+    get '/posts' do 
+
+    end 
+
+    #show route for a single post
 end 
