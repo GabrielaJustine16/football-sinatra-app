dfc2b8bc363ef96bf44c3070983b220cf14855eb
GabrielaJustine16 <johnsongabriela12@gmail.com>
2020-08-26 21:29:46 -0700

    adds the log out to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 #render login form
     get "/login" do
-        erb :login
+        erb :'users/login'
     end
 
         #recieve the data. the params
@@ -24,6 +24,25 @@ class UsersController < ApplicationController
     end 
 
         get "/users/:id" do 
-            "users show page"
+            binding.pry
+            @user = User.find_by(id: params[:id])
+            erb :'users/show'
         end 
+        #sign up
+        #get sign up rout that renders signup form
+        get '/signup' do 
+
+            #render signup form
+            erb :'/users/signup'
+        end 
+
+        post '/users' do 
+            #will eventually need to add validation to confirm all inputs are filled out befor creating user
+            @user = User.create(params)
+
+
+        #post sign up route that rendures data from user create the user, and user id
+
+        #log out
+        #get logout that clears the sesion
     end 
--- db/migrate/20200819055005_create_posts.rb
+++ db/migrate/20200819055005_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :video_url
+      t.string :highlight_url
       t.text :description
       t.integer :user_id
 
