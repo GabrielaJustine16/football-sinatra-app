bd919efe84f7c5d29bf5788a53d601c26b9b2de1
GabrielaJustine16 <johnsongabriela12@gmail.com>
2020-08-27 21:57:11 -0700

    adds editing routes to the post controllersalso adds flash messages. still need to check and see if everything is added up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,27 @@ class PostsController < ApplicationController
     end 
     #create
     get "/posts/new" do 
+        redirect_if_not_logged_in
 
-        erb :"posts/new"
+        erb :'posts/new'
+    end 
+
+    #post route for post 
+    post "/posts" do 
+#recieve the params that the user put into the create form
+    #active record method to create this post and eventually we nee d the user id. we will get it by leveraging
+        post = Post.new(title: params[:title], highlight_url: params[:highlight_url],desctription: params[:description],user_id: current_user.id )
+        if @post.save
+            flash[:message] = "Post created!"    
+            
+            
+            #redirect to post show page 
+        redirect "/posts/#{post.id}"
+        #can be a plain ruby variable and we can leverage the id. we added the post= 
+    else
+        flash[:errors] = "Post creation failure: #{@post.errors.full_messages.to_sentence}"
+        redirect '/posts/new'
+      end
     end 
 
     #show route for a single post
@@ -24,15 +43,51 @@ class PostsController < ApplicationController
     #params is how we access id
     get 'posts/:id' do 
         #find the post
-        @post = Post.find(params[:id])
-        erb :"/posts/show"
-
-
-    end 
+        # id is coming from url - params
+    find_post
+    erb :'posts/show'
+  end
 
     #CREATE
 
     #UPDATE
+    get '/posts/:id/edit' do 
+        redirect_if_not_logged_in
+        find_post
+        if authorized_to_edit?(@post)
+          erb :'/posts/edit'
+        else
+          flash[:errors] = "Not authorized to edit post."
+          redirect "/posts/#{@post.id}"
+        end
+      end
 
-    #DELETE
-end 
+    # patch route to update a existing post
+  patch '/posts/:id' do
+    find_post
+    @post.update(title: params[:title], highlight_url: params[:highlight_url], description: params[:description])
+    redirect "/posts/#{@post.id}"
+  end
+
+  # DELETE
+  # delete rotue to delete and existing post
+  delete '/posts/:id' do
+    # we need the id to FIND the post to delete
+    find_post
+    if authorized_to_edit?(@post)
+      @post.destroy
+      flash[:message] = "Successfully deleted post!"
+      redirect '/posts'
+    else
+      flash[:errors] = "You're not authorized to delete this post."
+      redirect "/posts/#{@post.id}"
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+end
